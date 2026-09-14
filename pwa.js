17180fd8f4fba1ecad0050dc9dbfa33845287e22
Rezlivemarketing <marketingrezlive@gmail.com>
2026-09-14 10:33:06 +0530

    Register service worker within repository scope

--- pwa.js
+++ pwa.js
@@ -4,7 +4,7 @@
 
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js').catch(console.error);
+      navigator.serviceWorker.register('./sw.js', { scope: './' }).catch(console.error);
     });
   }
 
